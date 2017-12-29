f8495a6a3669139d28388883ed7146c24240d48f
e.kacilo <e.kacilo@urancompany.com>
2017-12-29 18:06:21 +0200

    fix login form

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import { Jumbotron, Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Label, Input, Col } from 'reactstrap';
+import {Alert, Form, Jumbotron, Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Label, Input, Col } from 'reactstrap';
 import {Link} from 'react-router';
 import {loginUser} from '../../actions';
 
@@ -11,8 +11,14 @@ class Home extends Component {
       modal: false,
       backdrop: 'static',
       email: '',
-      password: ''
+      password: '',
+      visible: false,
+      errorText: ''
     };
+  }
+
+  onDismiss = () => {
+    this.setState({ visible: false });
   }
 
   toggle = () => {
@@ -21,17 +27,33 @@ class Home extends Component {
     });
   }
 
-  handleLogin = (e) => {
-    const data = {
-      email: this.state.email,
-      password: this.state.password
-    }
-    this.props.loginUser(data)
+  handleSubmit = (e) => {
+    const email = this.state.email
+    const password = this.state.password
 
-    this.setState({
-      modal: !this.state.modal
+    if (email === '' || password === '') {
+      this.setState({
+        visible: true,
+        errorText: 'Make sure you fill all the fields!'
+      })
+      return null
+    }
+
+    if (!email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) {
+      this.setState({
+        visible: true,
+        errorText: 'No valid email!'
+      })
+      return null
+    }
+
+    this.props.loginUser({
+      email,
+      password
     })
+    this.toggle()
   }
+
   handleEmail = (e) => {
     this.setState({
       email: e.target.value
@@ -61,6 +83,10 @@ class Home extends Component {
         <Modal isOpen={this.state.modal} toggle={this.toggle} backdrop={this.state.backdrop}>
           <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
           <ModalBody>
+            <Alert color="danger" isOpen={this.state.visible} toggle={this.onDismiss}>
+              {this.state.errorText}
+            </Alert>
+            <Form>
             <FormGroup row>
               <Label for="exampleEmail" sm={2}>Email</Label>
               <Col sm={10}>
@@ -73,9 +99,10 @@ class Home extends Component {
                 <Input type="password" onChange={this.handlePassword} name="password" id="examplePassword" placeholder="password placeholder" />
               </Col>
             </FormGroup>
+            </Form>
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" onClick={this.handleLogin}>Do Something</Button>{' '}
+            <Button color="primary" onClick={this.handleSubmit}>Do Something</Button>{' '}
             <Button color="secondary" onClick={this.toggle}>Cancel</Button>
           </ModalFooter>
         </Modal>
